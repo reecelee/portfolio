2cade3f6ea1b85aa62e6ca10098904031da278ae
Reece Lee <reecelee0128@gmail.com>
2025-07-14 15:52:15 -0700

    first commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Reece Lee</h1>
+      <h2>A coder from Hawaii looking to connect the world!</h2>
+    </>
+  )
+}
+
+export default App
