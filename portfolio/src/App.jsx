9eb616e434d852f6e4ceab1e0ec819e221d552dc
Reece Lee <reecelee0128@gmail.com>
2025-07-17 15:12:02 -0700

    Add scrollable elements

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Reece Lee</h1>
-      <h2>A coder from Hawaii looking to connect the world!</h2>
-    </>
+      <div className='scrollmenu'>
+        <a href="">Element 1</a>
+        <a href="">Element 1</a>
+        <a href="">Element 1</a>
+        <a href="">Element 1</a>
+        <a href="">Element 1</a>
+        <a href="">Element 1</a>
+        <a href="">Element 1</a>
+        <a href="">Element 1</a>
+        <a href="">Element 1</a>
+        <a href="">Element 1</a>
+      </div>
+\    </>
   )
 }
 
